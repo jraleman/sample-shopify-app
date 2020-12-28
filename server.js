61d46ea246b75b59b9edad5abe1fa877c85ebccc
jraleman <jraleman@bendiburg.com>
2020-12-27 16:48:09 -0800

    Set createShopifyAuth and verifyRequest middleware

--- server.js
+++ server.js
@@ -14,4 +14,28 @@ const dev = NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-app.prepare().then(() => {});
+app.prepare().then(() => {
+    const server = new Koa();
+    server.use(session({ secure: true, sameSite: 'none' }, server));
+    server.keys = [SHOPIFY_API_SECRET_KEY];
+    server.use(async (ctx) => {
+      await handle(ctx.req, ctx.res);
+      ctx.respond = false;
+      ctx.res.statusCode = 200;
+    });
+    server.use(
+        createShopifyAuth({
+          apiKey: SHOPIFY_API_KEY,
+          secret: SHOPIFY_API_SECRET_KEY,
+          scopes: ['read_products'],
+          afterAuth(ctx) {
+            const { shop, accessToken } = ctx.session;
+            ctx.redirect('/');
+          },
+        }),
+      );
+    server.use(verifyRequest());
+    server.listen(port, () => {
+        console.log(`> Ready on http://localhost:${port}`);
+    });
+});
