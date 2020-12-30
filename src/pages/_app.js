9400e43086ffb682b62ce5783e79e58f55be9e12
jraleman <jraleman@bendiburg.com>
2020-12-29 22:10:08 -0800

    Import i18n translations and props

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,7 @@ import App from 'next/app';
 import Head from 'next/head';
 import { AppProvider } from '@shopify/polaris';
 import '@shopify/polaris/dist/styles.css';
+import translations from '@shopify/polaris/locales/en.json';
 
 const App = ({ Component, pageProps }) => (
     <>
@@ -9,7 +10,7 @@ const App = ({ Component, pageProps }) => (
             <title>Sample App</title>
             <meta charSet="utf-8" />
         </Head>
-        <AppProvider>
+        <AppProvider i18n={translations}>
             <Component { ...pageProps }/>
         </AppProvider>
     </>
