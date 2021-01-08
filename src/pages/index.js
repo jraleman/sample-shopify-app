e1a9533a7bb2443262a6bfbc5e6b0868d0db34bf
jraleman <jraleman@bendiburg.com>
2021-01-07 23:29:01 -0800

    Add EmptyState component and example text to Polaris page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,15 @@
-import { Layout, Page, TextStyle } from '@shopify/polaris';
+import { 
+    EmptyState,
+    Layout,
+    Page,
+    TextStyle
+} from '@shopify/polaris';
+
+const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
+const action = {
+    content: 'Select products',
+    onAction: () => console.log('clicked'),
+};
 
 const Index = () => (
     <Page>
@@ -6,6 +17,13 @@ const Index = () => (
             <TextStyle variation="positive">
                 Sample app using React and Next.js
             </TextStyle>
+            <EmptyState
+                heading="Discount your products temporarily"
+                action={action}
+                image={img}
+            >
+                <p>Select products to change their price temporarily.</p>
+            </EmptyState>
         </Layout>
     </Page>
 );
