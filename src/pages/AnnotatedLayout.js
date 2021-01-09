98f9ff0e0d61cc2918fe0a46b16fafe5a9cd2c64
jraleman <jraleman@bendiburg.com>
2021-01-08 23:31:18 -0800

    Use Polaris Annotated Layout component

--- src/pages/AnnotatedLayout.js
+++ src/pages/AnnotatedLayout.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Card, Layout, Page } from '@shopify/polaris';
 
 const AnnotatedLayout = () => (
-    <div>
-        Annotated layout page
-    </div>
+    <Page>
+        <Layout>
+            <Layout.AnnotatedSection
+                title="Default discount"
+                description="Add a product to Sample App, it will automatically be discounted."
+            >
+                <Card sectioned>
+                    <div>
+                        Annotated layout page
+                    </div>
+                </Card>
+            </Layout.AnnotatedSection>
+        </Layout>
+    </Page>
 );
 
 export default AnnotatedLayout;
