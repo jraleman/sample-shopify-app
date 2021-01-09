e155bd55576079ff577c5853954bcc81cdab4372
jraleman <jraleman@bendiburg.com>
2021-01-08 23:39:07 -0800

    Add discount field and fields state to AnnotatedLayout

--- src/pages/AnnotatedLayout.js
+++ src/pages/AnnotatedLayout.js
@@ -1,21 +1,55 @@
-import React from 'react';
-import { Card, Layout, Page } from '@shopify/polaris';
+import React, { useState } from 'react';
+import {
+    Button,
+    Card,
+    Form,
+    FormLayout,
+    Layout,
+    Page,
+    Stack,
+    TextField,
+} from '@shopify/polaris';
 
-const AnnotatedLayout = () => (
-    <Page>
-        <Layout>
+const AnnotatedLayout = () => {
+    const [fields, setFields] = useState({
+        discount: '10%',
+    });
+
+    const handleSubmit = () => {
+        setFields({...fields, discount});
+        console.log('submission', fields);
+    };
+
+    const handleChange = (field) => (value) => setFields({ [field]: value });
+
+    return (
+        <Page>
+            <Layout>
             <Layout.AnnotatedSection
                 title="Default discount"
                 description="Add a product to Sample App, it will automatically be discounted."
             >
                 <Card sectioned>
-                    <div>
-                        Annotated layout page
-                    </div>
+                <Form onSubmit={handleSubmit}>
+                    <FormLayout>
+                    <TextField
+                        value={discount}
+                        onChange={handleChange('discount')}
+                        label="Discount percentage"
+                        type="discount"
+                    />
+                    <Stack distribution="trailing">
+                        <Button primary submit>
+                        Save
+                        </Button>
+                    </Stack>
+                    </FormLayout>
+                </Form>
                 </Card>
             </Layout.AnnotatedSection>
-        </Layout>
-    </Page>
-);
+            </Layout>
+        </Page>
+    );
+};
 
 export default AnnotatedLayout;
