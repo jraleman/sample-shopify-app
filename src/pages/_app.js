f119ace2e460b4272a75285bceb2021746192d03
jraleman <jraleman@bendiburg.com>
2021-01-18 19:06:33 -0800

    Add shopOrigin and forceRedirect props to providers

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,19 +5,28 @@ import translations from '@shopify/polaris/locales/en.json';
 import '@shopify/polaris/dist/styles.css';
 import { Provider } from '@shopify/app-bridge-react';
 
-const App = ({ Component, pageProps }) => (
-    <>
-        <Head>
-            <title>Sample App</title>
-            <meta charSet="utf-8" />
-        </Head>
-        <Provider>
-            <AppProvider i18n={translations}>
-                <Component { ...pageProps }/>
-            </AppProvider>
-        </Provider>
-    </>
-);
+const {SHOPIFY_API_KEY: apiKey} = process.env;
+
+const App = ({ Component, pageProps, shopOrigin }) => {
+    const config = { 
+        apiKey, 
+        shopOrigin, 
+        forceRedirect: true 
+    };
+    return (
+        <>
+            <Head>
+                <title>Sample App</title>
+                <meta charSet="utf-8" />
+            </Head>
+            <Provider config={config}>
+                <AppProvider i18n={translations}>
+                    <Component { ...pageProps }/>
+                </AppProvider>
+            </Provider>
+        </>
+    );
+};
 
 App.getInitialProps = async ({ ctx }) => {
     return {
