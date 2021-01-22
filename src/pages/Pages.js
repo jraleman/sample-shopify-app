1d6f4bee83e493b3cf8a4cb4d79c25d271066f07
jraleman <jraleman@bendiburg.com>
2021-01-21 21:56:15 -0800

    Add TitleBar component to navbar

--- src/pages/Pages.js
+++ src/pages/Pages.js
@@ -4,15 +4,21 @@ import {
     Page,
     TextStyle
 } from '@shopify/polaris';
+import { TitleBar } from '@shopify/app-bridge-react';
 
 const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
 const action = {
     content: 'Select products',
     onAction: () => console.log('clicked'),
 };
+const titlePrimaryAction = { content: 'Select products' };
 
 const Pages = () => (
     <Page>
+        <TitleBar
+            title="Sample App"
+            primaryAction={titlePrimaryAction}
+        />
         <Layout>
             <TextStyle variation="positive">
                 Sample app using React and Next.js
