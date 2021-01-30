dc6ce2a0e5bd06406a85070e3c2fdf511b9fa980
jraleman <jraleman@bendiburg.com>
2021-01-30 08:22:53 -0800

    Add shopify's app bridge resource picker

--- src/pages/Pages.js
+++ src/pages/Pages.js
@@ -1,37 +1,56 @@
+import React, { useState } from 'react';
 import { 
     EmptyState,
     Layout,
     Page,
     TextStyle
 } from '@shopify/polaris';
-import { TitleBar } from '@shopify/app-bridge-react';
+import { TitleBar, ResourcePicker } from '@shopify/app-bridge-react';
 
-const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
-const action = {
-    content: 'Select products',
-    onAction: () => console.log('clicked'),
+const Pages = () => {
+    const [isResourceOpen, setIsResourceOpen] = useState(false);
+    
+    const openResource = () => setIsResourceOpen(true);
+    const closeResource = () => setIsResourceOpen(false);
+    const toggleResource = () => setIsResourceOpen(r => !r);
+    const handleSelection = (resource) => {
+        closeResource();
+        console.log('resource', resource);
+    };
+
+    const action = {
+        content: 'Select products',
+        onAction: openResource,
+    };
+    const titlePrimaryAction = { content: 'Select products' };
+    const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
+    return (
+        <Page>
+            <TitleBar
+                title="Sample App"
+                primaryAction={titlePrimaryAction}
+            />
+            <ResourcePicker
+                resourceType="Product"
+                showVariants={false}
+                open={isResourceOpen}
+                onSelection={handleSelection}
+                onCancel={closeResource}
+            />
+            <Layout>
+                <TextStyle variation="positive">
+                    Sample app using React and Next.js
+                </TextStyle>
+                <EmptyState
+                    heading="Discount your products temporarily"
+                    action={action}
+                    image={img}
+                >
+                    <p>Select products to change their price temporarily.</p>
+                </EmptyState>
+            </Layout>
+        </Page>
+    );
 };
-const titlePrimaryAction = { content: 'Select products' };
-
-const Pages = () => (
-    <Page>
-        <TitleBar
-            title="Sample App"
-            primaryAction={titlePrimaryAction}
-        />
-        <Layout>
-            <TextStyle variation="positive">
-                Sample app using React and Next.js
-            </TextStyle>
-            <EmptyState
-                heading="Discount your products temporarily"
-                action={action}
-                image={img}
-            >
-                <p>Select products to change their price temporarily.</p>
-            </EmptyState>
-        </Layout>
-    </Page>
-);
 
 export default Pages;
